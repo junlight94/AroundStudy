3cd08405f9da412465b13a2c8f7d112d5b6e6dba
Junyoung <junlight94@gmail.com>
2022-10-01 21:12:01 +0900

    ASUS-9 작업중 - 커스텀 탭바 작업중

--- AroundStudy/View/RootTabViewController.swift
+++ AroundStudy/View/RootTabViewController.swift
@@ -9,21 +9,49 @@ import UIKit
 
 class RootTabViewController: UIViewController {
 
+    @IBOutlet weak var viewContainer: UIView!
+    
+    var selectedTabIndex = 0
+    
+    lazy var viewControllers = [
+        HomeViewController(nibName: "HomeViewController", bundle: nil),
+        StudyViewController(nibName: "StudyViewController", bundle: nil),
+        ChattingViewController(nibName: "ChattingViewController", bundle: nil),
+        MoreViewController(nibName: "MoreViewController", bundle: nil)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - General Function
+    func changeTap(view: Int) {
+        if let sub = viewContainer.subviews.first {
+            sub.removeFromSuperview()
+        }
+        
+        let vc = viewControllers[view]
+        
+        addChild(vc)
+        
+        viewContainer.addSubview(vc.view)
+        
+        vc.view.snp.makeConstraints { make in
+            make.centerX.centerY.width.height.equalToSuperview()
+        }
+        
+        vc.didMove(toParent: self)
     }
-    */
+    
+    //MARK: - Selector Function
+        
+    //MARK: - IBAction Function
+    @IBAction func btnChangeTab(_ sender: UIButton) {
+        changeTap(view: sender.tag)
+    }
+    
+
 
 }
+//MARK: - Extension
