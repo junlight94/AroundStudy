1711dec7f0c7b1ddd1a9bc5e75dee6cc5d63b943
ParkSangHyeon <ios.humhae@gmail.com>
2022-10-02 13:20:21 +0900

    ASUS-11 수정, 팝업 공통함수 선언

--- AroundStudy/ViewController.swift
+++ AroundStudy/ViewController.swift
@@ -14,12 +14,4 @@ class ViewController: BaseViewController {
         super.viewDidLoad()
         print("스플래쉬 화면")
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        let testVC = TestViewController(nibName: "TestViewController", bundle: nil)
-        testVC.modalTransitionStyle = .crossDissolve
-        testVC.modalPresentationStyle = .fullScreen
-        self.present(testVC, animated: false)
-    }
 }
