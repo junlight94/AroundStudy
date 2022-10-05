b6d742fd72e855c36d62fd1fc7396983618fc59f
Junyoung <junlight94@gmail.com>
2022-10-05 20:12:30 +0900

    ASUS-24 완료 - NavigationController 작업완료, 탭바 이미지 작업 완료

--- AroundStudy/View/RootTabViewController.swift
+++ AroundStudy/View/RootTabViewController.swift
@@ -11,12 +11,23 @@ class RootTabViewController: UIViewController {
 
     @IBOutlet weak var viewContainer: UIView!
     
+    @IBOutlet weak var imgHomeTab: UIImageView!
+    @IBOutlet weak var imgStudyTab: UIImageView!
+    @IBOutlet weak var imgChatTab: UIImageView!
+    @IBOutlet weak var imgMoreTab: UIImageView!
+    
+    
     lazy var viewControllers = [
         HomeViewController(nibName: "HomeViewController", bundle: nil),
         StudyViewController(nibName: "StudyViewController", bundle: nil),
         ChattingViewController(nibName: "ChattingViewController", bundle: nil),
         MoreViewController(nibName: "MoreViewController", bundle: nil)
     ]
+    
+    let tabIcon = [["home_inactive","home_active"], ["study_inactive","study_active"],
+                   ["chat_inactive", "chat_active"], ["more_inactive","more_active"]]
+    
+    lazy var imgTab = [imgHomeTab, imgStudyTab, imgChatTab, imgMoreTab]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +49,17 @@ class RootTabViewController: UIViewController {
         addChild(vc)
         
         viewContainer.addSubview(vc.view)
+        
+        for i in 0..<imgTab.count {
+            guard let imgView = imgTab[i] else { return }
+            
+            if i == view {
+                imgView.image = UIImage(named: tabIcon[i][1])
+            } else {
+                imgView.image = UIImage(named: tabIcon[i][0])
+            }
+            
+        }
         
         vc.view.snp.makeConstraints { make in
             make.leading.trailing.top.bottom.equalToSuperview()
