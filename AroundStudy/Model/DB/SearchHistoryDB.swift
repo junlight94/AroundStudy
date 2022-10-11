79ee52e4a26af72318c6bd7f3a2a0ae785b3c0f0
Junyoung <junlight94@gmail.com>
2022-10-11 20:06:04 +0900

    ASUS-31 작업중 - SearchHistory DB (realm) 생성

---
+++ AroundStudy/Model/DB/SearchHistoryDB.swift
@@ -0,0 +1,8 @@
+//
+//  SearchHistoryDB.swift
+//  AroundStudy
+//
+//  Created by Junyoung on 2022/10/11.
+//
+
+import Foundation
--- AroundStudy/View/Common/SearchHistoryCollectionViewCell.swift
+++ AroundStudy/View/Common/SearchHistoryCollectionViewCell.swift
@@ -11,6 +11,7 @@ class SearchHistoryCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var viewContent: UIView!
     @IBOutlet weak var lbTitle: UILabel!
     @IBOutlet weak var btnDelete: UIButton!
+    @IBOutlet weak var btnOnClick: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
