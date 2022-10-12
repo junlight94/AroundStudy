8a4afee92add9ae763e6a6fed455a663b188ea25
JeongSungGyu <84212041+coder3306@users.noreply.github.com>
2022-10-12 09:35:15 +0900

    ASUS-38 [22.10.12] 1. 커스텀 피커 뷰 작업 중 2. 불필요한 메인 스토리보드 버튼 삭제

--- AroundStudy/View/CustomPickerTableViewCell.swift
+++ AroundStudy/View/CustomPickerTableViewCell.swift
@@ -13,6 +13,10 @@ class CustomPickerTableViewCell: UITableViewCell, reusableTableView {
     var hour = [String]()
     var minute = [String]()
     
+    /**
+     * @커스텀 피커 뷰 셀 초기화
+     * @creator : coder3306
+     */
     override func awakeFromNib() {
         super.awakeFromNib()
         timePickerView?.delegate = self
@@ -20,6 +24,18 @@ class CustomPickerTableViewCell: UITableViewCell, reusableTableView {
         setTimeData()
     }
     
+    /**
+     * @레이아웃 정의
+     * @creator : coder3306
+     */
+    override func layoutSubviews() {
+        initLayout()
+    }
+    
+    /**
+     * @피커 뷰 시간 설정
+     * @creator : coder3306
+     */
     private func setTimeData() {
         for hour in 0 ..< 24 {
             if hour < 10 {
@@ -28,7 +44,6 @@ class CustomPickerTableViewCell: UITableViewCell, reusableTableView {
                 self.hour.append("\(hour)")
             }
         }
-        
         for minute in 0 ..< 60 {
             if minute < 10 {
                 self.minute.append("0\(minute)")
@@ -39,6 +54,7 @@ class CustomPickerTableViewCell: UITableViewCell, reusableTableView {
     }
     
     private func initLayout() {
+        timePickerView?.backgroundColor = .clear
         let upLine = UIView(frame: CGRect(x: 15, y: 0, width: 56, height: 2))
         let underLine = UIView(frame: CGRect(x: 15, y: 60, width: 56, height: 2))
         upLine.backgroundColor = UIColor(named: "naver")
@@ -52,11 +68,20 @@ class CustomPickerTableViewCell: UITableViewCell, reusableTableView {
     }
 }
 
+//MARK: - UIPickerViewDataSource, UIPickerViewDelegate
 extension CustomPickerTableViewCell: UIPickerViewDataSource, UIPickerViewDelegate {
+    /**
+     * @커스텀 피커 뷰 선택 가능한 갯수 설정
+     * @creator : coder3306
+     */
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
     }
     
+    /**
+     * @커스텀 피커 뷰 데이터 초기화
+     * @creator : coder3306
+     */
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if component == 0 {
             return hour.count
@@ -64,6 +89,14 @@ extension CustomPickerTableViewCell: UIPickerViewDataSource, UIPickerViewDelegat
         return minute.count
     }
     
+    /**
+     * @커스텀 피커 뷰 제목 설정
+     * @creator : coder3306
+     * @param <# param #> : <# desc #>
+     * @param <# param #> : <# desc #>
+     * @param <# param #> : <# desc #>
+     * @Return : <# desc #>
+     */
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if component == 0 {
             return hour[row]
@@ -71,6 +104,10 @@ extension CustomPickerTableViewCell: UIPickerViewDataSource, UIPickerViewDelegat
         return minute[row]
     }
     
+    /**
+     * @시간이 선택되었을 때 해야 할 행동 전달
+     * @creator : coder3306
+     */
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         print(row)
     }
