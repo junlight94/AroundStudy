4806242cd1fddcd4c9b2cbfda20b725e2c69d9d1
ParkSangHyeon <ios.humhae@gmail.com>
2022-10-24 14:40:11 +0900

    ASUS-57, 사진선택 UI 변경

--- AroundStudy/View/PhotoPickerViewController.swift
+++ AroundStudy/View/PhotoPickerViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import Photos
-import PhotosUI
+import Mantis
 
 class PhotoPickerViewController: BaseViewController {
     
@@ -24,12 +24,19 @@ class PhotoPickerViewController: BaseViewController {
     let imageManager = PHCachingImageManager()
     /// 선택된 사진의 배열
     var selectedPhotos: [PhotoPicker] = []
+    /// 선택된 사진 인덱스
+    var selectedPhotoIndex: Int = 0
     
     
     /// 네비게이션바 버튼
     let backButton: UIButton = UIButton(type: .custom)
     let completeButton: UIButton = UIButton(type: .custom)
     
+    /// 제약 조건
+    @IBOutlet weak var previewCollectionViewHeightConstraint: NSLayoutConstraint!
+    
+    /// 컬렉션뷰
+    @IBOutlet weak var previewCollectionView: UICollectionView!
     @IBOutlet weak var photoPickerCollectionView: UICollectionView!
     
     override func viewDidLoad() {
@@ -63,7 +70,12 @@ extension PhotoPickerViewController {
      > coder : **sanghyeon**
      */
     func setupView() {
-        /// 컬렉션뷰 세팅
+        /// 사진 미리보기 컬렉션뷰 세팅
+        let previewCellNib = UINib(nibName: "PhotoPreviewCollectionViewCell", bundle: nil)
+        previewCollectionView.register(previewCellNib, forCellWithReuseIdentifier: "PhotoPreviewCollectionViewCell")
+        previewCollectionView.delegate = self
+        previewCollectionView.dataSource = self
+        /// 사진선택 컬렉션뷰 세팅
         let cellNib = UINib(nibName: "PhotoPickerCollectionViewCell", bundle: nil)
         photoPickerCollectionView.register(cellNib, forCellWithReuseIdentifier: "PhotoPickerCollectionViewCell")
         photoPickerCollectionView.delegate = self
@@ -106,9 +118,14 @@ extension PhotoPickerViewController {
      포토라이브러리 에셋 이미지 추출
     > coder : **sanghyeon**
      */
-    func fetchPhotos(index: Int, completion: @escaping (UIImage?) -> ()) {
+    func fetchPhotos(index: Int, highQuality: Bool = false, completion: @escaping (UIImage?) -> ()) {
         if let asset = allPhotos?.object(at: index) {
-            imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
+            let photosOptions = PHImageRequestOptions()
+            photosOptions.deliveryMode = .highQualityFormat
+            photosOptions.isSynchronous = true
+            photosOptions.isNetworkAccessAllowed = true
+            let photoTargetSize: CGSize = highQuality ? PHImageManagerMaximumSize: CGSize(width: 50, height: 50)
+            imageManager.requestImage(for: asset, targetSize: photoTargetSize, contentMode: .default, options: photosOptions, resultHandler: { image, info in
                 completion(image)
             })
         }
@@ -118,11 +135,18 @@ extension PhotoPickerViewController {
 //MARK: - CollectionView
 extension PhotoPickerViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return allPhotos?.count ?? 0 + 1
+        switch collectionView {
+        case previewCollectionView:
+            return selectedPhotos.count
+        case photoPickerCollectionView:
+            return allPhotos?.count ?? 0 + 1
+        default:
+            return 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return setupCell(indexPath: indexPath)
+        return setupCell(collectionView, indexPath: indexPath)
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -131,42 +155,56 @@ extension PhotoPickerViewController: UICollectionViewDelegate, UICollectionViewD
     
     /// 셀 사이즈
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let viewWidth = view.frame.width
-        let cellWidth = ceil((viewWidth - 6) / 3)
-        thumbnailSize = CGSize(width: cellWidth, height: cellWidth)
-        return thumbnailSize
+        if collectionView == photoPickerCollectionView {
+            let viewWidth = view.frame.width
+            let cellWidth = ceil((viewWidth - 6) / 3)
+            thumbnailSize = CGSize(width: cellWidth, height: cellWidth)
+            return thumbnailSize
+        } else {
+            return CGSize(width: 70, height: 70)
+        }
     }
     
     /**
      컬렉션뷰 셀 설정 기본함수 대체
      > coder : **sanghyeon**
      */
-    func setupCell(indexPath: IndexPath) -> UICollectionViewCell {
-        print(indexPath.item)
-        guard let cell = photoPickerCollectionView.dequeueReusableCell(withReuseIdentifier: "PhotoPickerCollectionViewCell", for: indexPath) as? PhotoPickerCollectionViewCell else { return UICollectionViewCell() }
-        if indexPath.row > 0 {
-            cell.setupCell(hideIcon: true, hideButton: isSingleSelectMode)
-            
-            if let asset = allPhotos?.object(at: indexPath.row - 1) {
-                cell.representedAssetIdentifier = asset.localIdentifier
-                fetchPhotos(index: indexPath.row - 1) { result in
-                    if let image = result {
-                        if cell.representedAssetIdentifier == asset.localIdentifier {
-                            cell.setImage(image: image)
+    func setupCell(_ collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
+        switch collectionView {
+        case previewCollectionView:
+            guard let cell = previewCollectionView.dequeueReusableCell(withReuseIdentifier: "PhotoPreviewCollectionViewCell", for: indexPath) as? PhotoPreviewCollectionViewCell else { return UICollectionViewCell() }
+            cell.setImage(image: selectedPhotos[indexPath.row].data.original)
+            return cell
+        case photoPickerCollectionView:
+            guard let cell = photoPickerCollectionView.dequeueReusableCell(withReuseIdentifier: "PhotoPickerCollectionViewCell", for: indexPath) as? PhotoPickerCollectionViewCell else { return UICollectionViewCell() }
+            if indexPath.row > 0 {
+                cell.setupCell(hideIcon: true, hideButton: isSingleSelectMode)
+                
+                if let asset = allPhotos?.object(at: indexPath.row - 1) {
+                    cell.representedAssetIdentifier = asset.localIdentifier
+                    fetchPhotos(index: indexPath.row - 1) { result in
+                        if let image = result {
+                            if cell.representedAssetIdentifier == asset.localIdentifier {
+                                cell.setImage(image: image)
+                            }
                         }
                     }
                 }
-            }
-            
-            if let _ = selectedPhotos.firstIndex(where: {$0.index == indexPath.row}) {
-                cell.selectCell(select: true)
+                
+                if let index = selectedPhotos.firstIndex(where: {$0.index == indexPath.row}) {
+                    cell.selectCell(select: true)
+                    cell.setIndex(index: index + 1)
+                } else {
+                    cell.selectCell(select: false)
+                    cell.setIndex(index: nil)
+                }
             } else {
-                cell.selectCell(select: false)
+                cell.setupCell(hideIcon: false, hideButton: true)
             }
-        } else {
-            cell.setupCell(hideIcon: false, hideButton: true)
+            return cell
+        default:
+            return UICollectionViewCell()
         }
-        return cell
     }
     
     /**
@@ -176,9 +214,8 @@ extension PhotoPickerViewController: UICollectionViewDelegate, UICollectionViewD
     func selectCell(indexPath: IndexPath) {
         switch indexPath.row {
         case 0:
-            print("selectedPhotos: \(self.selectedPhotos)")
+            return
         default:
-            print("셀 선택!")
             guard let cell = photoPickerCollectionView.cellForItem(at: indexPath) as? PhotoPickerCollectionViewCell else { return }
             
             if let targetIndex = selectedPhotos.firstIndex(where: {$0.index == indexPath.row}) {
@@ -189,21 +226,54 @@ extension PhotoPickerViewController: UICollectionViewDelegate, UICollectionViewD
                     print("최대 선택 개수를 초과함")
                     return
                 }
-                fetchPhotos(index: indexPath.row - 1) { result in
-                    print("선택된 사진 가져오기 완료")
+                fetchPhotos(index: indexPath.row - 1, highQuality: true) { result in
                     if let image = result {
                         let selectedPhoto: PhotoPicker = .init(index: indexPath.row, data: ImageData(original: image, processed: nil))
                         if let _ = self.selectedPhotos.firstIndex(where: {$0.index == indexPath.row}) { } else {
+                            /// 현재 선택된 사진의 인덱스 저장
+                            self.selectedPhotoIndex = indexPath.row
+                            /// Mantis CropViewController 설정
+                            let cropViewController = Mantis.cropCustomizableViewController(image: selectedPhoto.data.original)
+                            cropViewController.modalTransitionStyle = .crossDissolve
+                            cropViewController.modalPresentationStyle = .fullScreen
+                            cropViewController.delegate = self
+                            cell.selectCell(select: true)
+                            self.navigationController?.pushViewController(cropViewController, animated: true)
+                            
                             self.selectedPhotos.append(selectedPhoto)
                         }
-                        cell.selectCell(select: true)
                     }
                 }
             }
             
             DispatchQueue.main.async {
-                print("selectedPhotos: \(self.selectedPhotos)")
+                self.previewCollectionView.reloadData()
+                self.photoPickerCollectionView.reloadData()
             }
         }
     }
+}
+
+//MARK: - MantisDelegate
+extension PhotoPickerViewController: CropViewControllerDelegate {
+    func cropViewControllerDidCrop(_ cropViewController: Mantis.CropViewController, cropped: UIImage, transformation: Mantis.Transformation, cropInfo: Mantis.CropInfo) {
+        print("\(selectedPhotoIndex)번째 이미지가 크랍됨")
+        cropViewController.navigationController?.popViewController(animated: true)
+        /// 선택된 이미지 인덱스 초기화
+        selectedPhotoIndex = 0
+    }
+    
+    func cropViewControllerDidCancel(_ cropViewController: Mantis.CropViewController, original: UIImage) {
+        cropViewController.navigationController?.popViewController(animated: true)
+        guard let cell = photoPickerCollectionView.cellForItem(at: IndexPath(row: selectedPhotoIndex, section: 0)) as? PhotoPickerCollectionViewCell else { return }
+        if let targetIndex = selectedPhotos.firstIndex(where: {$0.index == selectedPhotoIndex}) {
+            selectedPhotos.remove(at: targetIndex)
+            cell.selectCell(select: false)
+        }
+    }
+    
+
+    
+    
+    
 }
